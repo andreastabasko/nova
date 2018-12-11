2ed90f5da1ec533dcf6d4339aff41c7513edebdb
ANDY <ANDY@10.80.3.175>
2018-12-10 17:47:35 -0800

    hgj

--- eclipse-workspace/York/src/main/java/New/York/App.java
+++ eclipse-workspace/York/src/main/java/New/York/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World and Planet!" );
+        System.out.println( "Hello World and Planetof Apes!" );
     }
 }
