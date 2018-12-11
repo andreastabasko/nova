f3a07c3d355927514834f42860061f580a879206
ANDY <ANDY@10.80.3.175>
2018-12-10 17:26:09 -0800

    Added Mother#ucker word

--- eclipse-workspace/York/src/main/java/New/York/App.java
+++ eclipse-workspace/York/src/main/java/New/York/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Amigoso!" );
+        System.out.println( "Hello World Amigoso Mother#ucker!" );
     }
 }
